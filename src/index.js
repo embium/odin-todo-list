9628f1af930122a6c37395fc2d91f094e42c4441
Michael Mooney <mikeymooney1991@gmail.com>
2023-06-07 11:12:33 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+
+function component() {
+  const element = document.createElement("div");
+  element.innerHTML = "Hello, webpack!";
+  element.classList.add("hello");
+  return element;
+}
+
+document.body.appendChild(component());
